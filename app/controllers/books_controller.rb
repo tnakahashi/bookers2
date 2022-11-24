92acbd70954878d6609aa56460beb365d3cd7342
EC2 Default User <ec2-user@ip-172-31-35-103.ap-northeast-1.compute.internal>
2022-11-24 10:11:52 +0000

    [Update]Ch12_削除機能追加前

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
+    redirect_to book_path(@book.id)
   end
   
   def index
@@ -16,6 +16,7 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])  
   end
   
   private
