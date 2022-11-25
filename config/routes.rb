a7619fd738eb04cc3bbac69c3bd7d117f02d429a
EC2 Default User <ec2-user@ip-172-31-35-103.ap-northeast-1.compute.internal>
2022-11-25 03:12:40 +0000

    [Update]Ch13_削除機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  resources :books, only: [:new, :create, :index, :show, :edit, :update]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   devise_for :users
   root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
