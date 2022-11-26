3c62b6200f7d541224ef0ffd41122f2731b07308
EC2 Default User <ec2-user@ip-172-31-35-103.ap-northeast-1.compute.internal>
2022-11-26 04:13:36 +0000

    [Fix]ユーザ新規登録に関するエラー解消後

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   
-  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
-  resources :users, only: [:show, :edit, :update]
+  
   devise_for :users
   root to: "homes#top"
+  
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
